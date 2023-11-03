9c748e18f8558f27d72bc172bde049836283361a
HanNadin <eranadine3540@gmail.com>
2023-11-03 10:13:41 +0700

    first commit

---
+++ Kelas Industri/belajar-next/pages/contact.js
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+
+const contact = () => {
+    return (
+        <>
+            <Navbar />
+            <h1>Ini halaman Contacts</h1>
+        </>
+    );
+};
+
+export default contact;
---
+++ Kelas Industri/belajar-next/pages/portofolio.js
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+
+const portofolio = () => {
+    return (
+        <>
+            <Navbar />
+            <h1>Ini halaman Portofolio</h1>
+        </>
+    );
+};
+
+export default portofolio;
